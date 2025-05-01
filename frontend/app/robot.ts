0d2cd675c4881da6070687cc5b3b63de9b72dff7
Rudraksh Agarwal <114608152+Rudy248@users.noreply.github.com>
2025-05-01 13:33:10 +0530

    changes sitemap url

--- frontend/app/robot.ts
+++ frontend/app/robot.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://apollo-clone.vercel.app/sitemap.xml",
+    sitemap: "http://localhost:3000/sitemap.xml",
   };
 }
