893641b736aa7a0a59c64e7776a1b7f4dbbe54a3
Rudraksh Agarwal <114608152+Rudy248@users.noreply.github.com>
2025-05-09 00:32:35 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 origins = ["http://localhost:3000", "https://doctor-listing-apollo-mtte.vercel.app"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
