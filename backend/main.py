e27ad01e17b86449814a104d0fb60d6a36c089c4
Rudraksh Agarwal <114608152+Rudy248@users.noreply.github.com>
2025-05-08 17:56:25 +0530

    updated files for deplyment

--- backend/main.py
+++ backend/main.py
@@ -152,3 +152,7 @@ def add_doctor(doctor: DoctorCreate):
     db.close()
 
     return {"message": "Doctor added successfully"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
